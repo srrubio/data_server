80122ee490b847cc6395dd91b8c3c22fa7919e7d
Sergio Rubio <sergio.rubio@soprasteria.com>
2023-08-03 12:06:56 +0200

    .

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000; //  chose port from here like 8080, 3001
+const port = process.env.PORT
 
 app.use(middlewares);
 app.use(router);
